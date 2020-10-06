5125d07c8d38cd85f6266dc17a7c226513ae1b6b
jeff-d56 <jeff_davidson56@mymail.eku.edu>
2020-10-06 03:58:02 -0400

    added gatsby react tic tac toe

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Game from "../components/game"
+import './index.css';
+
+export default function Home() {
+  return (
+    <div style={{ color: `teal` }}>
+	  <Game/>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
+
+
+
