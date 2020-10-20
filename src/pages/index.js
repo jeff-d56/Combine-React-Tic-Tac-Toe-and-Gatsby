dd99e431761f7adf1c6b2c1819d3eab4feb40274
jeff-d56 <jeff_davidson56@mymail.eku.edu>
2020-10-20 18:59:35 -0400

    added jest testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Game from "../components/TicTacToe/game"
+import Header from "../components/header"
 import './index.css';
 
 export default function Home() {
   return (
     <div style={{ color: `teal` }}>
+	  <Header/>
 	  <Game/>
-      <p>Such wow. Very React.</p>
+      
     </div>
   )
 }
